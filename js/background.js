33eb468a47c6c7f7094a145875eec732b97289e5
Joona Yoon <joonas.yoon@gmail.com>
2021-01-29 22:57:24 +0900

    feat: moved config to background

--- js/background.js
+++ js/background.js
@@ -1,13 +1,82 @@
-chrome.runtime.onMessage.addListener(function (message) {
+function openSettingPage() {
+  chrome.runtime.openOptionsPage();
+}
+
+class ConfigClass {
+  constructor() {
+    this.storage =
+      typeof browser !== 'undefined'
+        ? browser.storage.sync
+        : chrome.storage.sync;
+    this.storageName = 'config';
+    this.buffer = {};
+    const self = this;
+    this.storage.get(this.storageName, (items) => {
+      if (items[self.storageName]) {
+        self.buffer = items[self.storageName];
+      }
+    });
+  }
+
+  // can be bufferred
+  save(key, value, callback) {
+    this.buffer[key] = value;
+    const obj = {};
+    obj[this.storageName] = this.buffer;
+    this.storage.set(obj, () => {
+      if (typeof callback === 'function') callback(value);
+    });
+  }
+
+  load(key, callback) {
+    // find in buffer first (but not synchronized yet)
+    if (this.buffer[key] !== undefined) {
+      callback(this.buffer[key]);
+    } else {
+      const storageName = this.storageName;
+      this.storage.get(storageName, (items) => {
+        if (typeof callback !== 'function') return;
+        if (!items || !items[storageName]) {
+          callback(null);
+        } else {
+          callback(items[storageName][key]);
+        }
+      });
+    }
+  }
+
+  // callback(true/false := deleted as well)
+  remove(key, callback) {
+    if (this.buffer[key] !== undefined) {
+      delete this.buffer[key];
+      const obj = {};
+      obj[this.storageName] = this.buffer;
+      this.storage.set(obj, () => {
+        if (typeof callback === 'function') callback(true);
+      });
+    } else {
+      if (typeof callback === 'function') callback(false);
+    }
+  }
+}
+
+const Config = new ConfigClass(); // as singleton
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   switch (message.action) {
     case 'openSettingPage':
       openSettingPage();
+      break;
+    case 'config.save':
+      Config.save(message.data.key, message.data.value, sendResponse);
+      break;
+    case 'config.load':
+      Config.load(message.data.key, sendResponse);
+      break;
+    case 'config.remove':
+      Config.remove(message.data.key, sendResponse);
       break;
     default:
       break;
   }
 });
-
-function openSettingPage() {
-  chrome.runtime.openOptionsPage();
-}
--- js/config.js
+++ js/config.js
@@ -1,60 +1,40 @@
-'use strict';
-
 class ConfigClass {
-  constructor() {
-    this.storage =
-      typeof browser !== 'undefined'
-        ? browser.storage.sync
-        : chrome.storage.sync;
-    this.storageName = 'config';
-    this.buffer = { enable: false, lang: undefined }; // lang as i18n code like 'ko'
-    const self = this;
-    this.storage.get(this.storageName, (items) => {
-      if (items[self.storageName]) {
-        self.buffer = items[self.storageName];
-      }
-    });
-  }
-
   // can be bufferred
   save(key, value, callback) {
-    this.buffer[key] = value;
-    const obj = {};
-    obj[this.storageName] = this.buffer;
-    this.storage.set(obj, () => {
-      if (typeof callback === 'function') callback(value);
-    });
+    chrome.runtime.sendMessage(
+      {
+        action: 'config.save',
+        data: {
+          key: key,
+          value: value,
+        },
+      },
+      callback
+    );
   }
 
   load(key, callback) {
-    // find in buffer first (but not synchronized yet)
-    if (this.buffer[key] !== undefined) {
-      callback(this.buffer[key]);
-    } else {
-      const storageName = this.storageName;
-      this.storage.get(storageName, (items) => {
-        if (typeof callback !== 'function') return;
-        if (!items || !items[storageName]) {
-          callback(null);
-        } else {
-          callback(items[storageName][key]);
-        }
-      });
-    }
+    chrome.runtime.sendMessage(
+      {
+        action: 'config.load',
+        data: {
+          key: key,
+        },
+      },
+      callback
+    );
   }
 
-  // callback(true/false := deleted as well)
   remove(key, callback) {
-    if (this.buffer[key] !== undefined) {
-      delete this.buffer[key];
-      const obj = {};
-      obj[this.storageName] = this.buffer;
-      this.storage.set(obj, () => {
-        if (typeof callback === 'function') callback(true);
-      });
-    } else {
-      if (typeof callback === 'function') callback(false);
-    }
+    chrome.runtime.sendMessage(
+      {
+        action: 'config.remove',
+        data: {
+          key: key,
+        },
+      },
+      callback
+    );
   }
 }
 
