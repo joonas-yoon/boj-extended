f0c5522a266b11db2f310f6f28452d81c5c23d6d
Joona Yoon <joonas.yoon@gmail.com>
2022-01-07 19:04:47 +0900

    feat: add tabs on quick serach (#55)

--- js/search.js
+++ js/search.js
@@ -1,6 +1,5 @@
 function extendQuickSearch() {
-  let isActive = false;
-
+  // UI: overlay
   const bg = Utils.createElement('div', {
     id: 'quick-search',
     class: 'overlay',
@@ -30,14 +29,41 @@ function extendQuickSearch() {
   moreButton.innerText = '더 많은 검색 결과 보기';
   form.appendChild(input);
   form.appendChild(resultBox);
+  // UI: tabs
+  const tabsContainer = Utils.createElement('div', {
+    class: 'tabs',
+  });
+  const tabs = [
+    { title: '문제', c: 'Problems', active: true, el: null },
+    { title: '문제집', c: 'Workbooks', el: null },
+    { title: '출처', c: 'Categories', el: null },
+    { title: '블로그', c: 'Blogs', el: null },
+    { title: '게시판', c: 'Articles', el: null },
+  ];
+  for (let i = 0; i < tabs.length; ++i) {
+    const tab = tabs[i];
+    const tabEl = Utils.createElement('div', { class: 'tab', tabIndex: i });
+    tabEl.innerText = tab.title;
+    if (tab.active) tabEl.classList.add('active');
+    tabEl.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      activateTab(tabEl.getAttribute('tabIndex'));
+    });
+    tabs[i].el = tabEl; // refer
+    tabsContainer.appendChild(tabEl);
+  }
+  container.appendChild(tabsContainer);
   container.appendChild(form);
   container.appendChild(resultFooter);
   container.appendChild(moreButton);
   bg.appendChild(container);
   document.body.appendChild(bg);
 
+  // variables
   let searchHandle = null;
   let lastSearchText = '';
+  let problemInfo = {};
+  let currentTabIndex = 0;
 
   // add event listener to input
   input.addEventListener('keyup', async (evt) => {
@@ -53,6 +79,7 @@ function extendQuickSearch() {
     }
   });
 
+  // handle key event
   const keyPressed = new Set();
   document.addEventListener('keydown', (evt) => {
     keyPressed.add(evt.key);
@@ -60,34 +87,58 @@ function extendQuickSearch() {
   document.addEventListener('keyup', (evt) => {
     console.log(evt);
     if (keyPressed.has('Escape')) {
-      isActive = false;
-      bg.style.display = 'none';
+      activate(false);
     } else if (
       keyPressed.has('/') &&
       (keyPressed.has('Control') || keyPressed.has('Alt'))
     ) {
-      isActive = true;
-      bg.style.display = 'flex';
-      setTimeout(() => {
-        input.focus();
-      }, 10);
+      activate(true);
     }
     keyPressed.delete(evt.key);
   });
 
   // dismiss search overlay
   document.addEventListener('click', (evt) => {
-    if (evt.target == bg) {
-      isActive = false;
-      bg.style.display = 'none';
-    }
+    if (evt.target == bg) activate(false);
   });
 
+  async function activate(on) {
+    if (on === true) {
+      // fetch problem status by current user
+      problemInfo = await fetchProblemsByUser(getMyUsername());
+      // set variables
+      bg.style.display = 'flex';
+      setTimeout(() => {
+        input.focus();
+      }, 10);
+    } else {
+      // deactivate
+      bg.style.display = 'none';
+    }
+  }
+
+  function activateTab(tabIndex) {
+    for (const tab of tabs) {
+      const isActive = tab.el.getAttribute('tabIndex') == tabIndex;
+      if (isActive) {
+        tab.el.classList.add('active');
+      } else {
+        tab.el.classList.remove('active');
+      }
+    }
+    currentTabIndex = Number(tabIndex);
+    search(input.value);
+  }
+
   async function search(searchText) {
+    // scroll to top
+    resultBox.scroll(0, 0);
+    // hijack
+    const currentIndexName = tabs[currentTabIndex].c || 'Problems';
     const dataForm = {
       requests: [
         {
-          indexName: 'Problems',
+          indexName: currentIndexName,
           params: encodeURI(
             'query=' + searchText + '&page=0&facets=[]&tagFilters='
           ),
@@ -103,29 +154,118 @@ function extendQuickSearch() {
       }
     ).then((res) => res.json());
 
-    resultBox.innerHTML = '';
     const { hits, processingTimeMS, nbHits } = results[0];
-    console.log(results[0]);
+    resultBox.innerHTML = '';
+    console.groupCollapsed(`${currentIndexName}: "${searchText}"`);
+    console.log('results', results[0]);
     for (const res of hits) {
-      const { id, time, memory } = res;
-      const { title, description } = res._highlightResult;
-      const item = Utils.createElement('div', { class: 'quick-search-item' });
-      item.innerHTML = `\
-        <div class="title"><a href="/problem/${id}">${id}번 - ${title.value}</a></div>\
-        <div class="meta">시간 제한: ${time}초 &nbsp; 메모리 제한: ${memory}MB</div>\
-        <div class="desc">${description.value}</div>\
-        <div class="links"> \
-          <a href="/submit/${id}">제출</a> \
-          <a href="/problem/status/${id}">맞은 사람</a> \
-          <a href="/status?from_problem=1&amp;problem_id=${id}">채점 현황</a> \
-        </div> \
-      `;
-      resultBox.appendChild(item);
+      const item = createResultItem(res, currentIndexName);
+      if (item) resultBox.appendChild(item);
+      console.log(res);
     }
-    moreButton.href = encodeURI('/search#q=' + searchText + '&c=Problems');
+    console.groupEnd();
+    moreButton.href = encodeURI('/search#q=' + searchText + '&c=' + currentIndexName);
     resultFooter.innerHTML = `${nbHits}개의 결과 중 ${hits.length}개 표시 (${
       processingTimeMS / 1000
     }초)`;
+  }
+
+  function createResultItem(result, indexName) {
+    if (result == null) return null;
+    switch (indexName) {
+      case 'Problems':
+        return createItemFromHTML(htmlProblems(result));
+      case 'Workbooks':
+        return createItemFromHTML(htmlWorkbooks(result));
+      case 'Categories':
+        return createItemFromHTML(htmlCategories(result));
+      case 'Blogs':
+        return createItemFromHTML(htmlBlogs(result));
+      case 'Articles':
+        return createItemFromHTML(htmlArticles(result));
+      default:
+        return null;
+    }
+  }
+
+  function createItemFromHTML(html) {
+    const item = Utils.createElement('div', { class: 'quick-search-item' });
+    item.innerHTML = html;
+    return item;
+  }
+
+  function htmlProblems(result) {
+    const { id, time, memory, _highlightResult } = result;
+    const { title, description } = _highlightResult;
+    return `\
+      <div class="title"><a href="/problem/${id}">${id}번 - ${title.value}</a></div>\
+      <div class="meta">시간 제한: ${time}초 &nbsp; 메모리 제한: ${memory}MB</div>\
+      <div class="desc">${description.value}</div>\
+      <div class="links"> \
+        <a href="/submit/${id}">제출</a> \
+        <a href="/problem/status/${id}">맞은 사람</a> \
+        <a href="/status?from_problem=1&amp;problem_id=${id}">채점 현황</a> \
+      </div> \
+    `;
+  }
+  function htmlWorkbooks(result) {
+    const { id, problems, _highlightResult } = result;
+    const { name, comment, creator, problem } = _highlightResult;
+    return `\
+      <div class="title"><a href="/workbook/view/${id}">${name.value}</a></div>\
+      <div class="meta">만든 사람: ${creator.value} &nbsp; 문제: ${problems}</div>\
+      <div class="desc">${comment.value}</div>\
+    `;
+  }
+  function htmlCategories(result) {
+    const { avail, id, parents, total, _highlightResult } = result;
+    const { name } = _highlightResult;
+    const breadcrumb = (parents || []).map(child => `<li><a href="/category/${child.id}">${child.name}</a></li>`).join('\n');
+    return `\
+      <ul class="list-inline up-ul search-breadcrumb">${breadcrumb}</ul>
+      <div class="title"><a href="/category/detail/view/${id}">${name.value}</a></div>\
+      <div class="meta">전체 문제: ${total} &nbsp; 풀 수 있는 문제: ${avail}</div>\
+    `;
+  }
+  function htmlBlogs(result) {
+    const { id, user, comments, date, tags } = result;
+    const { title } = result._highlightResult;
+    const { content } = result._snippetResult;
+    const tagList = tags.filter(tag => tag.length > 0).map(tag => `<span class="tag">#${tag}</span>`).join('\n');
+    return `\
+      <div class="title"><a href="/blog/view/${id}">${title.value}</a></div>\
+      <div class="meta">\
+        <span class="date">${date}</span>\
+        <a class="author" href="/user/${user}">${user}</a>\
+        <a href="/blog/view/${id}#comments"><i class="fa fa-comments-o"></i> ${comments}</a>\
+      </div>\
+      <div class="desc">${content.value}</div>\
+      <div class="links">${tagList}</div> \
+    `;
+  }
+  function htmlArticles(result) {
+    const { id, problem, category, created, user, comments, like } = result;
+    const { subject } = result._highlightResult;
+    const { content } = result._snippetResult;
+    let problemTag = '';
+    let problemColor = '';
+    if (problem != null) {
+      problemColor = problemInfo[problem] || '';
+      problemTag = `<a href="/problem/${problem}" class="${problemColor}">${problem}번</a>&nbsp;`;
+    }
+    return `\
+      <div class="meta ${problemColor}">\
+        ${problemTag}<span>${category}</span>\
+      </div>
+      <div class="title"><a href="/board/view/${id}">${subject.value}</a></div>\
+      <div class="meta">\
+        <span class="date">${created}</span>\
+        <a class="author" href="/user/${user}">${user}</a>\
+        <a href="/board/view/${id}#comments" class="comments"><i class="fa fa-comments-o"></i> ${comments}</a>\
+        <span><i class="fa fa-thumbs-o-up"></i> ${like}</span>\
+      </div>\
+      <div class="desc">${content.value}</div>\
+    `;
   }
 }
 
