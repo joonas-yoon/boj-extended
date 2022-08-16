372375dd489c90abe3c0c43819e35d751882015c
Joona Yoon <joonas.yoon@gmail.com>
2022-08-17 01:55:03 +0900

    fix: missing path on static pages

--- options/option-timer.js
+++ options/option-timer.js
@@ -1,4 +1,4 @@
-(function () {
+(async function () {
   Array.from(document.getElementsByClassName('btn-close-window')).forEach(
     (e) => {
       e.addEventListener('click', (evt) => {
@@ -10,6 +10,17 @@
 
   const container = document.getElementById('problem-timer-list');
 
+  const PROBLEM_DB = await chrome.storage.local.get(Constants.BG_DB_PROBLEMS);
+  const getProblemName = (pid) => {
+    try {
+      const dbJson = JSON.parse(PROBLEM_DB[Constants.BG_DB_PROBLEMS]);
+      return dbJson['problems'][pid];
+    } catch (error) {
+      console.error(error);
+      return '<문제 제목을 불러오는 데 실패했습니다>';
+    }
+  };
+
   // Timers
   Config.load('problem-timers', (list) => {
     const pids = Object.keys(list || {});
@@ -17,7 +28,7 @@
       for (let i = 0; i < pids.length; ++i) {
         const pid = pids[i];
         const timeInfo = list[pid];
-        const pname = PROVISIONED_DB['problems'][pid];
+        const pname = getProblemName(pid);
         container.appendChild(createTimerElement(pid, pname, timeInfo));
       }
     } else {
@@ -31,14 +42,11 @@
   function createTimerElement(pid, pname, timeInfo) {
     const div = document.createElement('div');
     const label = document.createElement('label');
-    label.innerHTML =
-      '<a href="https://www.acmicpc.net/problem/' +
-      pid +
-      '">' +
-      pid +
-      '번 ' +
-      pname +
-      '</a>';
+    const a = Utils.createElement('a', {
+      href: `https://www.acmicpc.net/problem/${pid}`,
+    });
+    a.innerText = `${pid}번 ${pname}`;
+    label.appendChild(a);
     div.appendChild(label);
 
     const progress = progressTimer();
