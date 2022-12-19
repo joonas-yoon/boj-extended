cfd791d1dcf75f8a113589ef009c0af0c5c47ea7
Joona Yoon <joonas.yoon@gmail.com>
2022-12-19 12:37:38 +0900

    refactor: organize directories and files

--- build.sh
+++ build.sh
@@ -1,23 +1,20 @@
 #!/usr/bin/env bash
 # clean
 npm run clean
-rm -f lint.err
 # run babel
-env BABEL_ENV=production babel js/ --out-dir js/build
-env BABEL_ENV=production babel options/ --out-dir options/build
+env BABEL_ENV=production babel src/js/ --out-dir src/js/build
+env BABEL_ENV=production babel src/options/ --out-dir src/options/build
 # lint
+rm -f lint.err
 npm run lint 2> lint.err
 if [ -s lint.err ]; then
   cat lint.err
   exit 1
 fi
 # copy to dist
-cp -r css dist/css
-cp -r js/build dist/js
-cp -r icons dist/icons
-cp -r options dist/
+cp -r src/* dist
 mv dist/options/build/* dist/options/
 rmdir dist/options/build/
-cp rules.json dist
-cp manifest.json dist
 cp LICENSE dist
+# clean up
+rm -f lint.err
--- js/preload.js
+++
@@ -1,7 +0,0 @@
-// preload theme from localStorage
-(() => {
-  const html = document.documentElement;
-  let theme = localStorage.getItem(Constants.CONFIG_THEME);
-  if (!theme) theme = localStorage.getItem(Config.getKey('theme')); // v1.7.8 compatibility
-  html.setAttribute('theme', theme || 'light');
-})();
