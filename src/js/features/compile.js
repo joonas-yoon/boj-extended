22303658430110022c38ff038d01f0d1366b27f2
joonas <joonas.yoon@gmail.com>
2023-03-08 01:41:40 +0900

    feat: add compile button

--- src/js/features/compile.js
+++ src/js/features/compile.js
@@ -6,4 +6,34 @@
   }
 
   console.log('Ready to compile');
+
+  const submitButton = document.getElementById('submit_button');
+  const compileButton = createCompileButton();
+  submitButton.parentNode.appendChild(compileButton);
+
+  function createCompileButton() {
+    const button = Utils.createElement('button', {
+      type: 'button',
+      class: 'btn',
+      style: 'margin-left: 5px',
+    });
+    const staticTextarea = document.querySelector('textarea#source');
+    const dynamicTextarea = document.querySelector('.CodeMirror textarea');
+    dynamicTextarea.value = staticTextarea.value;
+    button.textContent = '컴파일';
+    button.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      const codeLines = document.querySelectorAll('.CodeMirror-code .CodeMirror-line[role="presentation"]');
+      const sourceCodeText = Array.from(codeLines).map(e => e.textContent).join('\n');
+      const result = compile(sourceCodeText || '');
+      window.alert('컴파일 결과: ' + result);
+    });
+    return button;
+  }
+
+  function compile(text) {
+    // TODO: implement
+    console.log('Start compile:', text);
+    return 'Good';
+  }
 })();
