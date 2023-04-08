240bf193f5dc37afbdb398802db42a41fd461a48
joonas <joonas.yoon@gmail.com>
2023-04-08 17:35:55 +0900

    feat: print result of compile in HTML

--- src/js/features/compile.js
+++ src/js/features/compile.js
@@ -2,34 +2,34 @@ const TIO_LANGUAGES_MAP = {
   'C++17': 'cpp-gcc',
   'Java 8': 'java-jdk',
   'Python 3': 'python3-cython',
-  'C11': 'c-gcc',
-  'PyPy3': 'python3-pypy',
-  'C99': 'c-gcc',
+  C11: 'c-gcc',
+  PyPy3: 'python3-pypy',
+  C99: 'c-gcc',
   'C++98': 'cpp-gcc',
   'C++11': 'cpp-gcc',
   'C++14': 'cpp-gcc',
   'Java 8 (OpenJDK)': 'java-openjdk',
   'Java 11': 'java-jdk',
   'C++20': 'cpp-gcc',
-  'Ruby': 'ruby',
+  Ruby: 'ruby',
   'Kotlin (JVM)': 'kotlin',
-  'Swift': 'swift4',
-  'Text': 'unsupported',
+  Swift: 'swift4',
+  Text: 'unsupported',
   'C#': 'cs-core',
   'node.js': 'javascript-node',
-  'Go': 'go',
+  Go: 'go',
   'Go (gccgo)': 'go',
   'Java 15': 'java-jdk',
-  'D': 'd',
+  D: 'd',
   'D (LDC)': 'd',
-  'PHP': 'php',
+  PHP: 'php',
   'Rust 2015': 'rust',
   'Rust 2018': 'rust',
   'Rust 2021': 'rust',
-  'Pascal': 'pascal-fpc',
-  'Scala': 'scala',
-  'Lua': 'lua',
-  'Perl': 'perl5',
+  Pascal: 'pascal-fpc',
+  Scala: 'scala',
+  Lua: 'lua',
+  Perl: 'perl5',
   'F#': 'fs-core',
   'Visual Basic': 'vb-core',
   'Objective-C': 'objective-c-gcc',
@@ -41,36 +41,36 @@ const TIO_LANGUAGES_MAP = {
   'C11 (Clang)': 'c-clang',
   'C++17 (Clang)': 'cpp-clang',
   'C++20 (Clang)': 'cpp-clang',
-  'Golfscript': 'golfscript',
-  'C90': 'c-gcc',
-  'C2x': 'c-gcc',
+  Golfscript: 'golfscript',
+  C90: 'c-gcc',
+  C2x: 'c-gcc',
   'C90 (Clang)': 'c-clang',
   'C2x (Clang)': 'c-clang',
-  'TypeScript': 'typescript',
+  TypeScript: 'typescript',
   'Assembly (32bit)': 'unsupported',
   'Assembly (64bit)': 'assembly-nasm',
-  'Bash': 'bash',
-  'Fortran': 'fortran-gfortran',
-  'Scheme': 'scheme-chez',
-  'Ada': 'ada-gnat',
-  'awk': 'awk',
-  'OCaml': 'ocaml',
+  Bash: 'bash',
+  Fortran: 'fortran-gfortran',
+  Scheme: 'scheme-chez',
+  Ada: 'ada-gnat',
+  awk: 'awk',
+  OCaml: 'ocaml',
   'Brainf**k': 'brainfuck',
-  'Whitespace': 'whitespace',
-  'Tcl': 'tcl',
-  'Rhino': 'unsupported',
-  'Cobol': 'cobol-gnu',
-  'Pike': 'pike',
-  'sed': 'sed-gnu',
-  'INTERCAL': 'intercal',
-  'bc': 'bc',
+  Whitespace: 'whitespace',
+  Tcl: 'tcl',
+  Rhino: 'unsupported',
+  Cobol: 'cobol-gnu',
+  Pike: 'pike',
+  sed: 'sed-gnu',
+  INTERCAL: 'intercal',
+  bc: 'bc',
   'Algol 68': 'algol68g',
-  'Befunge': 'befunge',
-  'FreeBASIC': 'unsupported',
-  'Haxe': 'haxe',
-  'LOLCODE': 'lolcode',
-  '아희': 'unsupported',
-  'SystemVerilog': 'unsupported',
+  Befunge: 'befunge',
+  FreeBASIC: 'unsupported',
+  Haxe: 'haxe',
+  LOLCODE: 'lolcode',
+  아희: 'unsupported',
+  SystemVerilog: 'unsupported',
 };
 
 (function extendCompile() {
@@ -83,50 +83,79 @@ const TIO_LANGUAGES_MAP = {
   console.log('Ready to compile');
 
   const submitButton = document.getElementById('submit_button');
-  const compileButton = createCompileButton();
+  const formGroup = submitButton.closest('.form-group');
+  const resultWrapper = Utils.createElement('div', {
+    class: 'col-md-offset-2 col-md-10',
+    style: 'margin-top: 1em',
+  });
+  console.log('submitButton', submitButton);
+  console.log('formGroup', formGroup);
+  console.log('resultWrapper', resultWrapper);
+  const resultPreBox = Utils.createElement('pre', {
+    style: 'display: none',
+  });
+  resultWrapper.appendChild(resultPreBox);
+  formGroup.appendChild(resultWrapper);
+  const compileButton = createCompileButton({
+    whenCompileRequested: () => {
+      resultPreBox.style.display = 'block';
+      resultPreBox.innerText = 'Compile...';
+    },
+    whenCompileDone: ({ stdout, stderr }) => {
+      resultPreBox.innerText = 'stdout:\n' + stdout;
+      resultPreBox.innerText += '\n-----------\nstderr:\n' + stderr;
+    },
+  });
   submitButton.parentNode.appendChild(compileButton);
 
-  function createCompileButton() {
+  function createCompileButton({ whenCompileRequested, whenCompileDone }) {
     const button = Utils.createElement('button', {
       type: 'button',
       class: 'btn',
       style: 'margin-left: 5px',
     });
-    const staticTextarea = document.querySelector('textarea#source');
-    const dynamicTextarea = document.querySelector('.CodeMirror textarea');
-    if (!dynamicTextarea.value) {
-      dynamicTextarea.value = staticTextarea.value;
-    }
-    button.textContent = '컴파일';
+    button.textContent = '컴파일 (beta)';
     button.addEventListener('click', async (evt) => {
       evt.preventDefault();
+      whenCompileRequested();
       const { stdout, stderr } = await compile();
-      alert("stdout:\n------\n" + stdout);
-      alert("stderr+timing:\n------\n" + stderr);
+      whenCompileDone({ stdout, stderr });
     });
     return button;
   }
 
   async function compile() {
+    // get source to compile
     const codeLines = document.querySelectorAll(
       '.CodeMirror-code .CodeMirror-line[role="presentation"]'
     );
-    const userSelectedLang = document
-      .getElementsByClassName('chosen-single')[0]
-      .textContent.trim();
-
     const sourceCodeText = Array.from(codeLines)
       .map((e) => e.textContent)
       .join('\n');
-    const compileLanguage = TIO_LANGUAGES_MAP[userSelectedLang];
 
-    if (!compileLanguage || compileLanguage === 'unsupported') {
+    // get language to compile
+    const userSelectedLangForm = document
+      .getElementById('language_chosen')
+      .getElementsByClassName('chosen-single')[0];
+    let compileLanguage;
+    if (!userSelectedLangForm) {
+      console.error('Can not found DOM Element #language_chosen');
       return {
-        stdout: null,
-        stderr: '지원하지 않는 언어입니다.',
+        stdout: '',
+        stderr: '페이지에 문제가 발생했습니다. 새로고침 후 다시 시도해주세요.',
       };
+    } else {
+      const selectedLang = userSelectedLangForm.textContent.trim();
+      compileLanguage = TIO_LANGUAGES_MAP[selectedLang];
+      if (!compileLanguage || compileLanguage === 'unsupported') {
+        return {
+          stdout: '',
+          stderr: '지원하지 않는 언어입니다.',
+        };
+      }
     }
 
+    // clean bad characters
     const unexpectedUnicodes =
       // eslint-disable-next-line no-control-regex
       /[\u000d\u0020\u0085\u1680\u2000-\u200a\u2029\u202f\u205f\u3000]+/g;
