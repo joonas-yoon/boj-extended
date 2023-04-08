3af5f9a9d70fdc68fca677c22ade7df67743426f
smartwe <githubdev.smartwe@gmail.com>
2023-04-08 15:51:56 +0900

    Feature/compile (#145)
    * feat: add compile feature (except dropdown)
    
    * chore: lint and pretty correct
    
    * feat: add exception of unsupported language
    
    * Update src/js/features/compile.js
    
    ---------
    
    Co-authored-by: Joona Yoon <joonas-yoon@users.noreply.github.com>

--- src/js/features/compile.js
+++ src/js/features/compile.js
@@ -1,4 +1,4 @@
-(function extendCompile() {
+(async function extendCompile() {
   const url = window.location.pathname;
 
   if (!url.startsWith('/submit/')) {
@@ -8,10 +8,11 @@
   console.log('Ready to compile');
 
   const submitButton = document.getElementById('submit_button');
-  const compileButton = createCompileButton();
+  const compileButton = await createCompileButton();
+  const compileDropdown = await createCompileDropdown();
   submitButton.parentNode.appendChild(compileButton);
 
-  function createCompileButton() {
+  async function createCompileButton() {
     const button = Utils.createElement('button', {
       type: 'button',
       class: 'btn',
@@ -21,7 +22,7 @@
     const dynamicTextarea = document.querySelector('.CodeMirror textarea');
     dynamicTextarea.value = staticTextarea.value;
     button.textContent = '컴파일';
-    button.addEventListener('click', (evt) => {
+    button.addEventListener('click', async (evt) => {
       evt.preventDefault();
       const codeLines = document.querySelectorAll(
         '.CodeMirror-code .CodeMirror-line[role="presentation"]'
@@ -29,15 +30,116 @@
       const sourceCodeText = Array.from(codeLines)
         .map((e) => e.textContent)
         .join('\n');
-      const result = compile(sourceCodeText || '');
-      window.alert('컴파일 결과: ' + result);
+      const input = '12 3'; // TODO: 입력값 받아오기
+      await buttonClick(input, sourceCodeText);
     });
     return button;
   }
 
-  function compile(text) {
-    // TODO: implement
-    console.log('Start compile:', text);
-    return 'Good';
+  async function buttonClick(input, code) {
+    const result = await compile(code, input);
+
+    await alert(result[0] + '\n' + result[1]);
+  }
+
+  async function compile(code, input) {
+    // eslint-disable-next-line camelcase
+    const lang_json = {
+      'C++17': 'cpp-gcc',
+      'Java 8': 'java-jdk',
+      'Python 3': 'python3-cython',
+      C11: 'c-gcc',
+      PyPy3: 'python3-pypy',
+      C99: 'c-gcc',
+      'C++98': 'cpp-gcc',
+      'C++11': 'cpp-gcc',
+      'C++14': 'cpp-gcc',
+      'Java 8 (OpenJDK)': 'java-openjdk',
+      'Java 11': 'java-jdk',
+      'C++20': 'cpp-gcc',
+      Ruby: 'ruby',
+      'Kotlin (JVM)': 'kotlin',
+      Swift: 'swift4',
+      Text: 'unsupported',
+      'C#': 'cs-core',
+      'node.js': 'javascript-node',
+      Go: 'go',
+      'Go (gccgo)': 'go',
+      'Java 15': 'java-jdk',
+      D: 'd',
+      'D (LDC)': 'd',
+      PHP: 'php',
+      'Rust 2015': 'rust',
+      'Rust 2018': 'rust',
+      'Rust 2021': 'rust',
+      Pascal: 'pascal-fpc',
+      Scala: 'scala',
+      Lua: 'lua',
+      Perl: 'perl5',
+      'F#': 'fs-core',
+      'Visual Basic': 'vb-core',
+      'Objective-C': 'objective-c-gcc',
+      'Objective-C++': 'unsupported',
+      'C99 (Clang)': 'c-clang',
+      'C++98 (Clang)': 'cpp-clang',
+      'C++11 (Clang)': 'cpp-clang',
+      'C++14 (Clang)': 'cpp-clang',
+      'C11 (Clang)': 'c-clang',
+      'C++17 (Clang)': 'cpp-clang',
+      'C++20 (Clang)': 'cpp-clang',
+      Golfscript: 'golfscript',
+      C90: 'c-gcc',
+      C2x: 'c-gcc',
+      'C90 (Clang)': 'c-clang',
+      'C2x (Clang)': 'c-clang',
+      TypeScript: 'typescript',
+      'Assembly (32bit)': 'unsupported',
+      'Assembly (64bit)': 'assembly-nasm',
+      Bash: 'bash',
+      Fortran: 'fortran-gfortran',
+      Scheme: 'scheme-chez',
+      Ada: 'ada-gnat',
+      awk: 'awk',
+      OCaml: 'ocaml',
+      'Brainf**k': 'brainfuck',
+      Whitespace: 'whitespace',
+      Tcl: 'tcl',
+      Rhino: 'unsupported',
+      Cobol: 'cobol-gnu',
+      Pike: 'pike',
+      sed: 'sed-gnu',
+      INTERCAL: 'intercal',
+      bc: 'bc',
+      'Algol 68': 'algol68g',
+      Befunge: 'befunge',
+      FreeBASIC: 'unsupported',
+      Haxe: 'haxe',
+      LOLCODE: 'lolcode',
+      아희: 'unsupported',
+      SystemVerilog: 'unsupported',
+    };
+    const lang =
+      lang_json[
+        document.getElementsByClassName('chosen-single')[0].textContent.trim()
+      ];
+
+    let result;
+
+    if (lang === 'unsupported') {
+      result = ['ERR', '지원하지 않는 언어입니다.'];
+      return result;
+    }
+
+    code = code.replace(
+      // eslint-disable-next-line no-control-regex
+      /[\u000d\u0020\u0085\u1680\u2000-\u200a\u2029\u202f\u205f\u3000]+/g,
+      ' '
+    );
+
+    return await TIO.run(code, input, lang);
+  }
+
+  function createCompileDropdown() {
+    // TODO : 컴파일 버튼 누르면 나오는 드롭다운 메뉴
   }
 })();
